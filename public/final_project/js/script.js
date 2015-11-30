eee9411c4fa4c9f565477c2a21bae9c748fe106c
bldesign <bensonlam@BL-MBA.local>
2015-11-30 18:19:18 +1100

    upload final_project to heroku

---
+++ public/final_project/js/script.js
@@ -0,0 +1,92 @@
+// images & icons
+var arrowDown = "img/arrow-down-512px.svg";
+
+var locateImg = "img/Purple-locate.svg";
+var twitterImg = "img/Purple-twitter.svg";
+var facebookImg = "img/Purple-FB.svg";
+var instagramImg = "img/Purple-instagram.svg";
+var googleImg = "img/Purple-google.svg";
+var pinterestImg = "img/Purple-pinterest.svg";
+var emailImg = "img/Purple-email.svg";
+var phoneImg = "img/Purple-phone.svg";
+
+var ingredient1Img = "img/raspberries.png";
+var ingredient2Img = "img/pomegranate.png";
+var ingredient3Img = "img/blueberries.png";
+
+var blog1Img = "img/Blog1.png";
+var blog2Img = "img/Blog2.png";
+var blog3Img = "img/Blog3.png";
+var blog4Img = "img/Blog4.png";
+var blog5Img = "img/Blog5.png";
+var blog6Img = "img/Blog6.png";
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.navbar').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('.navbar').removeClass('nav-down').addClass('nav-up');
+        $('.navbar-header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('.navbar').removeClass('nav-up').addClass('nav-down');
+            $('.navbar-header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+$().ready(function(){
+	$('.arrow-down').attr("src",arrowDown);
+	//ingredient images
+	$('#ingredient1-image').attr("src",ingredient1Img);
+	$('#ingredient1-modal-image').attr("src",ingredient1Img);
+	$('#ingredient2-image').attr("src",ingredient2Img);
+	$('#ingredient2-modal-image').attr("src",ingredient2Img);
+	$('#ingredient3-image').attr("src",ingredient3Img);
+	$('#ingredient3-modal-image').attr("src",ingredient3Img);
+
+	//social media icons
+	$('#locate-ico').attr("src",locateImg);
+	$('#twitter-ico').attr("src",twitterImg);
+	$('#fb-ico').attr("src",facebookImg);
+	$('#google-ico').attr("src",googleImg);
+	$('#insta-ico').attr("src",instagramImg);
+	$('#pinterest-ico').attr("src",pinterestImg);
+	$('#email-ico').attr("src",emailImg);
+	$('#phone-ico').attr("src",phoneImg);
+
+	//blog images
+	$('#blog1').attr("src",blog1Img);
+	$('#blog2').attr("src",blog2Img);
+	$('#blog3').attr("src",blog3Img);
+	$('#blog4').attr("src",blog4Img);
+	$('#blog5').attr("src",blog5Img);
+	$('#blog6').attr("src",blog6Img);
+})
